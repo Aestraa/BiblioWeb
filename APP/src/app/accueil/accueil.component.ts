fa559e4897216e30831d97ec8037660fb58ec511
BABEL Teddy <stephbab57@gmail.com>
2024-02-05 11:54:55 +0100

    Ajout acceuil CSS minimum

--- APP/src/app/accueil/accueil.component.ts
+++ APP/src/app/accueil/accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const currentSlide: any;
 
 @Component({
   selector: 'app-accueil',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
+  constructor(public rt: Router){}
+
+   pourcent:number[] = generateRandomNumbers(5);
+
+  event1:any = "assets/Images/scroll.jpg";
+  event2:any = "assets/Images/scroll.jpg";
+  event3:any = "assets/Images/scroll.jpg";
+  event4:any = "assets/Images/scroll.jpg";
+  vedette:any = "assets/Images/west.jpg";
+
+  ngOnInit() {
+    currentSlide(1)
+  }
+
+  getLeftPosition(index: number): number {
+    return 23 + index * 18; 
+  }
 
 }
+
+function generateRandomNumbers(length: number): number[] {
+  const randomNumbers: number[] = [];
+
+  for (let i = 0; i < length; i++) {
+    let randomNumber = Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    randomNumber = Math.floor(randomNumber / 5) * 5;
+    randomNumbers.push(randomNumber);
+  }
+
+  return randomNumbers;
+}
+
+
