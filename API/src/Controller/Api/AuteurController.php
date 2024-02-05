415589fac44a779b36305802860aaff3d993ab8f
Aestraa <moinyh42@gmail.com>
2024-02-05 11:36:33 +0100

    Add create methods for Auteur and Categorie controllers

--- API/src/Controller/Api/AuteurController.php
+++ API/src/Controller/Api/AuteurController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Auteur;
 use App\Repository\AuteurRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTimeImmutable;
 
 class AuteurController extends AbstractController
 {
@@ -14,5 +18,35 @@ class AuteurController extends AbstractController
     {
         $auteurs = $auteurRepository->findAll();
         return $this->json($auteurs, 200, [], ['groups' => 'auteur:read']);
+    }
+
+    #[Route('/api/auteur', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $auteur = new Auteur();
+        $auteur->setNom($data['Nom']);
+        $auteur->setPrenom($data['Prenom']);
+        $dateNaissance = new DateTimeImmutable($data['DateNaissance']);
+        $auteur->setDateNaissance($dateNaissance);
+
+        if (!empty($data['DateDeces'])) {
+            $dateDeces = new DateTimeImmutable($data['DateDeces']);
+            $auteur->setDateDeces($dateDeces);
+        }
+        
+        $auteur->setNationalite($data['Nationalite']);
+        $auteur->setPhoto($data['Photo']);
+        $auteur->setDescription($data['Description']);
+        
+        $date = new DateTimeImmutable("now");
+        $auteur->setCreatedAt($date);
+        $auteur->setUpdatedAt($date);
+
+        $entityManager->persist($auteur);
+        $entityManager->flush();
+
+        return $this->json($auteur, JsonResponse::HTTP_CREATED);
     }
 }
