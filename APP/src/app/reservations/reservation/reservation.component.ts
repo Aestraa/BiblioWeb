0a6bc73ab9bfd64f2a675779efcbc9fee0ced68d
Furiza31 <hugo.wendjaneh@gmail.com>
2024-02-08 11:34:23 +0100

    feat(reservation): show reservation and remove reservation

--- APP/src/app/reservations/reservation/reservation.component.ts
+++ APP/src/app/reservations/reservation/reservation.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Reservation } from '../../models/reservation';
+import { ApiService } from '../../services/api.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-reservation',
@@ -8,6 +11,28 @@ import { Reservation } from '../../models/reservation';
 })
 export class ReservationComponent {
   @Input() reservation!: Reservation;
+  reservationForm: FormGroup;
+  suppression = false;
 
-  constructor() {}
+  constructor(private api: ApiService, private auth: AuthService) {
+    this.reservationForm = new FormGroup({
+      id: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    let confirmation = confirm(
+      'Voulez-vous vraiment supprimer cette réservation ?'
+    );
+    if (confirmation) {
+      this.api
+        .deleteReservation({
+          ...this.reservationForm.value,
+          token: this.auth.token,
+        })
+        .subscribe(() => {
+          this.suppression = true;
+        });
+    }
+  }
 }
